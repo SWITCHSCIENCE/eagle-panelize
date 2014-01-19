a5c992f9c3246cb06a7119640c8b015251cf61df
Shigeru KANEMOTO <sgk@switch-science.com>
2014-01-20 03:57:24 +0900

    Add main() function.

--- panelize.py
+++ panelize.py
@@ -43,67 +43,73 @@ def offsetCopyChildren(dst, src, recursive):
       for elem in src:
         offsetCopy(dst, elem, x, y, recursive)
 
-parser = etree.XMLParser(remove_blank_text=True)
-src = etree.parse(sys.stdin, parser)
-eagle = src.getroot()
-if eagle.tag != 'eagle':
-  raise 'XML %s not supported.' % eagle.tag
+def panelize(src):
+  eagle = src.getroot()
+  if eagle.tag != 'eagle':
+    raise 'XML %s not supported.' % eagle.tag
 
-dsteagle = etree.Element('eagle')
-for k, v in eagle.items():
-  dsteagle.set(k, v)
-dst = etree.ElementTree(dsteagle)
+  dsteagle = etree.Element('eagle')
+  for k, v in eagle.items():
+    dsteagle.set(k, v)
+  dst = etree.ElementTree(dsteagle)
 
-partnames = etree.Element('partnames')
+  partnames = etree.Element('partnames')
 
-for child in eagle:
-  if child.tag == 'drawing':
-    drawing = child
-    dstdrawing = shallowCopy(dsteagle, child)
-    for child in drawing:
-      if child.tag == 'board':
-        board = child
-        dstboard = shallowCopy(dstdrawing, child)
-        for child in board:
-          if child.tag == 'plain':
-            dstplain = shallowCopy(dstboard, child)
-            offsetCopyChildren(dstplain, child, False)
-          elif child.tag == 'elements':
-            elements = child
-            dstelements = shallowCopy(dstboard, child)
-            for x in range(cols):
-              for y in range(rows):
-                for element in elements:
-                  offsetCopy(dstelements, element, x, y, False)
+  for child in eagle:
+    if child.tag == 'drawing':
+      drawing = child
+      dstdrawing = shallowCopy(dsteagle, child)
+      for child in drawing:
+        if child.tag == 'board':
+          board = child
+          dstboard = shallowCopy(dstdrawing, child)
+          for child in board:
+            if child.tag == 'plain':
+              dstplain = shallowCopy(dstboard, child)
+              offsetCopyChildren(dstplain, child, False)
+            elif child.tag == 'elements':
+              elements = child
+              dstelements = shallowCopy(dstboard, child)
+              for x in range(cols):
+                for y in range(rows):
+                  for element in elements:
+                    offsetCopy(dstelements, element, x, y, False)
 
-                  # Save <attribute> for later addition to <plain>
-                  attribute = element.find('attribute')
-                  if attribute != None:
-                    partname = etree.SubElement(partnames, 'text')
-                    partname.text = element.get('name')
-                    for k, v in attribute.items():
-                      if k == 'x':
-                        partname.set(k, str(float(v) + x * coloffset))
-                      elif k == 'y':
-                        partname.set(k, str(float(v) + y * rowoffset))
-                      elif k == 'name':
-                        pass
-                      else:
-                        partname.set(k, v)
+                    # Save <attribute> for later addition to <plain>
+                    attribute = element.find('attribute')
+                    if attribute != None:
+                      partname = etree.SubElement(partnames, 'text')
+                      partname.text = element.get('name')
+                      for k, v in attribute.items():
+                        if k == 'x':
+                          partname.set(k, str(float(v) + x * coloffset))
+                        elif k == 'y':
+                          partname.set(k, str(float(v) + y * rowoffset))
+                        elif k != 'name':
+                          partname.set(k, v)
 
-          elif child.tag == 'signals':
-            offsetCopyChildren(shallowCopy(dstboard, child), child, True)
-          else:
-            dstboard.append(child)
-      else:
-        dstdrawing.append(child)
-  else:
-    dsteagle.append(child)
+            elif child.tag == 'signals':
+              offsetCopyChildren(shallowCopy(dstboard, child), child, True)
+            else:
+              dstboard.append(child)
+        else:
+          dstdrawing.append(child)
+    else:
+      dsteagle.append(child)
 
-if dstplain != None:
-  for elem in partnames:
-    dstplain.append(elem)
+  if dstplain != None:
+    for elem in partnames:
+      dstplain.append(elem)
 
-print etree.tostring(
-    dst, encoding='UTF-8', xml_declaration=True,
-    pretty_print=True)
+  return dst
+
+def main():
+  parser = etree.XMLParser(remove_blank_text=True)
+  src = etree.parse(sys.stdin, parser)
+  dst = panelize(src)
+  print etree.tostring(
+      dst, encoding='UTF-8', xml_declaration=True,
+      pretty_print=True)
+
+if __name__ == '__main__':
+  main()
