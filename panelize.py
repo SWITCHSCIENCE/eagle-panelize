8b00bbc789613206d6e4e68ea0ed0a1777312c15
Shigeru KANEMOTO <sgk@accense.com>
2014-02-05 11:44:08 +0900

    Remove duplicate Vscore wires.

--- panelize.py
+++ panelize.py
@@ -7,6 +7,8 @@ import sys
 LAYER_DIMENSION = '20'
 LAYER_VSCORE = '102'
 
+VSCORE_OUT_LENGTH = 5.0 # mm
+
 # Copy src as child of dst.
 def shallowCopy(dst, src):
   elem = etree.SubElement(dst, src.tag)
@@ -21,14 +23,34 @@ def shallowCopy(dst, src):
 
 class Panelizer:
   def __init__(self, cols, rows, colspacing, rowspacing, hframe, vframe):
+    # Number of columns and rows.
     self.cols = cols
     self.rows = rows
+
+    # Board gap width.
     self.colspacing = colspacing # mm
     self.rowspacing = rowspacing # mm
+
+    # Frame width
     self.hframe = hframe # mm or zero
     self.vframe = vframe # mm or zero
+
+    # Offset distance to the next board.
     self.coloffset = 0.0 # mm
     self.rowoffset = 0.0 # mm
+
+    # Board dimension.
+    self.minx = 0.0 # mm
+    self.maxx = 0.0 # mm
+    self.miny = 0.0 # mm
+    self.maxy = 0.0 # mm
+    self.dimensionwidth = 0.0 # mm
+
+    # Panel dimension.
+    self.panelminx = 0.0 # mm
+    self.panelmaxx = 0.0 # mm
+    self.panelminy = 0.0 # mm
+    self.panelmaxy = 0.0 # mm
 
   # Copy src as child of dst.
   def offsetCopy(self, dst, src, x, y, recursive):
@@ -51,48 +73,72 @@ class Panelizer:
     return elem
 
   def copyPlain(self, dstplain, plain):
+    xs = dict()
+    ys = dict()
     for elem in plain:
       if elem.tag == 'wire' and elem.get('layer') == LAYER_VSCORE:
         # Process Vscore lines.
+        w = float(elem.get('width'))
+
+        # Check if vertical
         x1 = elem.get('x1')
         x2 = elem.get('x2')
+        if x1 == x2:
+          x = float(x1)
+          for i in range(self.cols):
+            xs[x] = max(xs.get(x, 0.0), w)
+            x += self.coloffset
+
+        # Check if horizontal
         y1 = elem.get('y1')
         y2 = elem.get('y2')
-        if x1 == x2:
-          # Vertical
-          y1f = float(y1)
-          y2f = float(y2)
-          if y1f > y2f:
-            y1 = str(y1f + self.rows * self.rowoffset - self.rowoffset)
-          else:
-            y2 = str(y2f + self.rows * self.rowoffset - self.rowoffset)
-          for x in range(self.cols):
-            x = str(float(x1) + x * self.coloffset)
-            e = shallowCopy(dstplain, elem)
-            e.set('x1', x)
-            e.set('x2', x)
-            e.set('y1', y1)
-            e.set('y2', y2)
-        elif y1 == y2:
-          # Horizontal
-          x1f = float(x1)
-          x2f = float(x2)
-          if x1f > x2f:
-            x1 = str(x1f + self.cols * self.coloffset - self.coloffset)
-          else:
-            x2 = str(x2f + self.cols * self.coloffset - self.coloffset)
-          for y in range(self.rows):
-            y = str(float(y1) + y * self.rowoffset)
-            e = shallowCopy(dstplain, elem)
-            e.set('x1', x1)
-            e.set('x2', x2)
-            e.set('y1', y)
-            e.set('y2', y)
+        if y1 == y2:
+          y = float(y1)
+          for i in range(self.rows):
+            ys[y] = max(ys.get(y, 0.0), w)
+            y += self.rowoffset
+        # Add the elements later.
+
       else:
         # Process other elements.
         for x in range(self.cols):
           for y in range(self.rows):
             self.offsetCopy(dstplain, elem, x, y, False)
+
+    # Function to add wire
+    def wire(x1, x2, y1, y2, width, layer):
+      elem = etree.SubElement(dstplain, 'wire')
+      elem.set('x1', x1)
+      elem.set('x2', x2)
+      elem.set('y1', y1)
+      elem.set('y2', y2)
+      elem.set('width', width)
+      elem.set('layer', layer)
+
+    # Add vertical Vscore wires.
+    y1 = str(self.panelminy - VSCORE_OUT_LENGTH)
+    y2 = str(self.panelmaxy + VSCORE_OUT_LENGTH)
+    for x, w in xs.items():
+      x = str(x)
+      wire(x, x, y1, y2, str(w), LAYER_VSCORE)
+
+    # Add vertical Vscore wires.
+    x1 = str(self.panelminx - VSCORE_OUT_LENGTH)
+    x2 = str(self.panelmaxx + VSCORE_OUT_LENGTH)
+    for y, w in ys.items():
+      y = str(y)
+      wire(x1, x2, y, y, str(w), LAYER_VSCORE)
+
+    # Add Dimension
+    x1 = str(self.panelminx)
+    x2 = str(self.panelmaxx)
+    y1 = str(self.panelminy)
+    y2 = str(self.panelmaxy)
+    w = str(self.dimensionwidth)
+    wire(x1, x1, y1, y2, w, LAYER_DIMENSION)
+    wire(x2, x2, y1, y2, w, LAYER_DIMENSION)
+    wire(x1, x2, y1, y1, w, LAYER_DIMENSION)
+    wire(x1, x2, y2, y2, w, LAYER_DIMENSION)
 
   def panelizeXML(self, src):
     eagle = src.getroot()
@@ -113,6 +159,7 @@ class Panelizer:
         xs.append(float(elem.get('x2')))
         ys.append(float(elem.get('y1')))
         ys.append(float(elem.get('y2')))
+        self.dimensionwidth = max(self.dimensionwidth, float(elem.get('width')))
     if len(xs) == 0 or len(ys) == 0:
       raise 'No dimension found.'
     self.minx = min(xs)
@@ -121,6 +168,12 @@ class Panelizer:
     self.maxy = max(ys)
     self.coloffset = (self.maxx - self.minx) + self.colspacing
     self.rowoffset = (self.maxy - self.miny) + self.rowspacing
+
+    # Panel dimension
+    self.panelminx = self.minx - self.vframe
+    self.panelmaxx = self.minx + self.coloffset * self.cols + self.vframe
+    self.panelminy = self.miny - self.hframe
+    self.panelmaxy = self.miny + self.rowoffset * self.rows + self.hframe
 
     # Copy root element <eagle>.
     dsteagle = etree.Element('eagle')
@@ -209,10 +262,14 @@ def main():
       help='Number of colmns (integer).')
   argparser.add_argument('--rows', type=int, required=True,
       help='Number of rows (integer).')
-  argparser.add_argument('--colspacing', type=float,
+  argparser.add_argument('--colspacing', type=float, default=0.0,
       help='Column spacing width in mm (float).')
-  argparser.add_argument('--rowspacing', type=float,
+  argparser.add_argument('--rowspacing', type=float, default=0.0,
       help='Row spacing height in mm (float).')
+  argparser.add_argument('--hframe', type=float, default=0.0,
+      help='Horizontal frame height in mm (float).')
+  argparser.add_argument('--vframe', type=float, default=0.0,
+      help='Vertical frame height in mm (float).')
   argparser.add_argument('--stdout', action='store_true', default=False,
       help='Write to stdout instead of file.')
   argparser.add_argument('file', type=str, nargs='+',
@@ -223,14 +280,17 @@ def main():
   if args.cols < 1 or args.rows < 1:
     print 'cols or rows not properly set.'
     sys.exit(1)
-  if args.colspacing < 0.0 or args.colspacing < 0.0:
+  if args.colspacing < 0.0 or args.rowspacing < 0.0:
     print 'colspacing or colspacing not properly set.'
+    sys.exit(1)
+  if args.hframe < 0.0 or args.vframe < 0.0:
+    print 'hframe or vframe not properly set.'
     sys.exit(1)
 
   panelizer = Panelizer(
     cols=args.cols, rows=args.rows,
     colspacing=args.colspacing, rowspacing=args.rowspacing,
-    hframe=0.0, vframe=0.0,
+    hframe=args.hframe, vframe=args.vframe,
   )
 
   for fname in args.file:
